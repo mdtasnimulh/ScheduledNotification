4d1f51ce222248cd77f0e339b08cb08714e1136b
unknown <Admin@IT-Tasnimul.bdjobs.local>
2022-10-01 17:24:06 +0600

    Initial commit

---
+++ app/src/main/java/com/example/schedulednotification/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.schedulednotification
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.schedulednotification.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        createNotificationChannel()
+        binding.submitButton.setOnClickListener {
+            scheduleNotification()
+        }
+    }
+
+    private fun scheduleNotification() {
+        val intent = Intent(applicationContext, Notification::class.java)
+        val title = binding.titleNt.text.toString()
+        val message = binding.messageNt.text.toString()
+        intent.putExtra("title", title)
+        intent.putExtra("message", message)
+//        val bundle = Bundle()
+//        //Add your data from getFactualResults method to bundle
+//        //Add your data from getFactualResults method to bundle
+//        bundle.putString("title", title)
+//        bundle.putString("message", message)
+//        //Add the bundle to the intent
+//        //Add the bundle to the intent
+//        intent.putExtras(bundle)
+//        startActivity(intent)
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            Notification.notificationId,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val time = getTime()
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            time,
+            pendingIntent
+        )
+        showAlert(time, title, message)
+    }
+
+    private fun showAlert(time: Long, title: String, message: String) {
+        val date = Date(time)
+        val dateFormat = android.text.format.DateFormat.getLongDateFormat(applicationContext)
+        val timeFormat = android.text.format.DateFormat.getTimeFormat(applicationContext)
+
+        AlertDialog.Builder(this)
+            .setTitle("Schedule Notification")
+            .setMessage(
+                "Title: " + title + "\nMessage: " + message + "\nAt: " + dateFormat.format(date) + " " +
+                        timeFormat.format(date)
+            )
+            .setPositiveButton("Okay") { _, _ -> }
+            .show()
+    }
+
+    private fun getTime(): Long {
+        val minute = binding.timePicker.minute
+        val hour = binding.timePicker.hour
+        val day = binding.datePicker.dayOfMonth
+        val month = binding.datePicker.month
+        val year = binding.datePicker.year
+        val calender = Calendar.getInstance()
+        calender.set(year, month, day, hour, minute)
+        return calender.timeInMillis
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Channel"
+            val description = "Channel Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(Notification.channelId, name, importance).apply {
+                description
+            }
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/schedulednotification/Notification.kt
@@ -0,0 +1,42 @@
+package com.example.schedulednotification
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.getIntent
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+
+
+class Notification: BroadcastReceiver() {
+
+    //val channelId = "mySchedule"
+  //  val notificationId = 1
+    val title = "Schedule Notification"
+    val message = "Message"
+    override fun onReceive(context: Context, intent: Intent) {
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(context).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_IMMUTABLE)
+        }
+        val notification = NotificationCompat.Builder(context, channelId)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(intent.getStringExtra(title))
+            .setContentText(intent.getStringExtra(message))
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(notificationId, notification)
+    }
+    companion object{
+        const val  notificationId = 1
+        const val channelId = "mySchedule"
+    }
+}
